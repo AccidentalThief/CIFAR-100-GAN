fa0c1fd298e9a952e9969db79644ced0ba1a906a
AccidentalThief <baisleymaxon@gmail.com>
2025-06-09 11:05:57 -0400

    Update Generator
    Trying a more powerful generator model

--- models/generator.py
+++ models/generator.py
@@ -6,15 +6,23 @@ class Generator(nn.Module):
         super().__init__()
         self.label_emb = nn.Embedding(n_classes, n_classes)
         self.init_size = img_size // 4
-        self.l1 = nn.Sequential(nn.Linear(latent_dim + n_classes, 128 * self.init_size ** 2))
+        self.l1 = nn.Sequential(
+            nn.Linear(latent_dim + n_classes, 256 * self.init_size ** 2),
+            nn.ReLU(True),
+            nn.Linear(256 * self.init_size ** 2, 128 * self.init_size ** 2),
+            nn.ReLU(True),
+        )
         self.conv_blocks = nn.Sequential(
             nn.BatchNorm2d(128),
             nn.Upsample(scale_factor=2),
             nn.Conv2d(128, 128, 3, stride=1, padding=1),
-            nn.BatchNorm2d(128, 0.8),
-            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
             nn.Upsample(scale_factor=2),
-            nn.Conv2d(128, output_channels, 3, stride=1, padding=1),
+            nn.Conv2d(128, 64, 3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(64, output_channels, 3, stride=1, padding=1),
             nn.Tanh(),
         )
 
